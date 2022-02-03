7ef270e9c83b4b2da7b11a673e14a4f99c9dd0cf
SEBASTIANREJI <98897891+SEBASTIANREJI@users.noreply.github.com>
2022-02-03 20:27:50 +0530

    Add files via upload

---
+++ Addition up to 10 number.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main()
+{
+  int  a[9],i,j;
+int sum=0;
+printf("Addition up to 10 numbers\n\n");
+printf("Enter up to 10 number");
+for(int i=0;i<10;i++)
+{
+scanf("%d",&a[i]);
+}
+for(int j=0;j<10;j++)
+{
+sum=sum+a[j];
+}
+printf("Sum is %d",sum);
+
+    return 0;
+}
